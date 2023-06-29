92b3ca5c5f8fc2ccfec97a274e185dd385a5c0ac
You Gao <henrygao00@gmail.com>
2023-06-29 17:12:23 -0400

    i have to stop eating out

--- foodplanner.py
+++ foodplanner.py
@@ -26,43 +26,50 @@ def remove_food():
         for line in lines:
             myfile.write(line)
 
-def cook_food():
-    with open("current food.csv", "r") as myfile:
-        lines = myfile.readlines()
-        for i in range(1, len(lines)):
-            item_line = lines[i].split(",")
-            print(str(i) + ": " + item_line[0])
-        index = input("Which item would you like to cook? ")
-        
-        # split the line into a list
-        item_line = lines[int(index)].split(",")
-        
-        item_line[3] = str(int(item_line[3]) - 1) # subtract one from the servings
-        # add the cost and calories to the total
-        item_cost = float(lines[int(index)].split(",")[5])
-        item_calories = float(lines[int(index)].split(",")[2])
-        if item_line[3] <= "0":
-            # write line to ate out.csv and remove from current food.csv
-            with open("ate out.csv", "a") as myfile:
-                myfile.write("Finished" + "," + item_line[1] + "," + item_line[4] + "\n")
-            myfile.close()
-            lines.pop(int(index))
-        else:
-            lines[int(index)] = ",".join(item_line) # join the list back into a string
-
-
-    myfile.close()
-    
-    # open the file again to write the new values
-    with open("current food.csv", "w") as myfile:
-        for line in lines:
-            myfile.write(line)
+def cook_food(amount):
+    week = input("Week: ")
+    item_calories = 0
+    item_cost = 0
+    for i in range(int(amount)):
+        with open("current food.csv", "r") as myfile:
+            lines = myfile.readlines()
+            for i in range(1, len(lines)):
+                item_line = lines[i].split(",")
+                print(str(i) + ": " + item_line[0])
+            index = input("Which item would you like to cook? ")
             
-    # write to consumption.csv
-    with open("consumption.csv", "a") as myfile:
-        week = input("Week: ")
-        day = input("Day: ")
-        myfile.write(item_line[0] + "," + str(item_calories) + "," + week + "," + day + "\n")
+            # split the line into a list
+            item_line = lines[int(index)].split(",")
+            
+            item_line[3] = str(int(item_line[3]) - 1) # subtract one from the servings
+            # add the cost and calories to the total
+            item_cos = float(lines[int(index)].split(",")[5])
+            item_cal = float(lines[int(index)].split(",")[2])
+            # add the cost and calories to the total
+            item_cost += item_cos
+            item_calories += item_cal
+            
+            if item_line[3] <= "0":
+                # write line to ate out.csv and remove from current food.csv
+                with open("ate out.csv", "a") as myfile:
+                    myfile.write("Finished" + "," + item_line[1] + "," + item_line[4] + "\n")
+                myfile.close()
+                lines.pop(int(index))
+            else:
+                lines[int(index)] = ",".join(item_line) # join the list back into a string
+
+
+        myfile.close()
+        
+        # open the file again to write the new values
+        with open("current food.csv", "w") as myfile:
+            for line in lines:
+                myfile.write(line)
+                
+        # write to consumption.csv
+        with open("consumption.csv", "a") as myfile:
+            day = input("Day: ")
+            myfile.write(item_line[0] + "," + str(item_cal) + "," + week + "," + day + "\n")
     return item_calories, item_cost
 
 def bought_food():
@@ -88,15 +95,9 @@ if __name__ == "__main__":
         for i in range(int(args[1])):
             remove_food()
     if args[0] == "cook":
-        total_calories = 0
-        total_cost = 0
-        for i in range(int(args[1])):
-            item_calories, item_cost = cook_food() # returns the calories and cost of the item also subtracts one from the servings
-
-            total_calories += item_calories
-            total_cost += item_cost
-        print("Total Calories: " + str(total_calories))
-        print("Total Cost: " + str(total_cost))
+        calories, cost = cook_food(args[1])
+        print("Total Calories: " + str(calories))
+        print("Total Cost: " + str(cost))
     if args[0] == "bought":
         for i in range(int(args[1])):
             bought_food()
